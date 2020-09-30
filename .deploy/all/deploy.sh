fc8b3522933278094d4a86a634937693ba90cbf4
Gabe Wyatt <gabe@solarixdigital.com>
2020-09-30 10:58:36 -0700

    ci: add CloudWatch logging to remote deployment
    ci: add AWS creds to CI vars

--- .deploy/all/deploy.sh
+++ .deploy/all/deploy.sh
@@ -2,6 +2,7 @@
 
 # Restart app
 ssh -o StrictHostKeyChecking=no "${REMOTE_USER}@${DEPLOY_ENDPOINT}" << EOF
-  pm2 start "cd ${TARGET_DIRECTORY} && yarn run start:${CI_ENVIRONMENT_NAME}"
+  cd ${TARGET_DIRECTORY}
+  pm2 start "NODE_ENV=${CI_ENVIRONMENT_NAME} node dist/server.js | ./node_modules/.bin/pino-cloudwatch --group coeus --stream coeus/base/${CI_ENVIRONMENT_NAME}/log --aws_region us-west-2 --aws_access_key_id ${AWS_ACCESS_KEY_ID} --aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
   sudo systemctl restart nginx
 EOF
