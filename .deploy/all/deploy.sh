bca11bf6335ec280bda94ebd15be4465ecd92a6b
Gabe Wyatt <gabe@solarixdigital.com>
2020-09-29 21:52:56 -0700

    ci: tweak app start command

--- .deploy/all/deploy.sh
+++ .deploy/all/deploy.sh
@@ -2,6 +2,6 @@
 
 # Restart app
 ssh -o StrictHostKeyChecking=no "${REMOTE_USER}@${DEPLOY_ENDPOINT}" << EOF
-  pm2 restart "cd ${TARGET_DIRECTORY} && yarn run start:${CI_ENVIRONMENT_NAME}"
+  pm2 start "cd ${TARGET_DIRECTORY} && yarn run start:${CI_ENVIRONMENT_NAME}"
   sudo systemctl restart nginx
 EOF
